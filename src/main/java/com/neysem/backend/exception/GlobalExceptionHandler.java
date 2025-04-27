6adf69f27886e47c7986d7acef1cbc624c410ccb
Selim Arda Çevik <arda.cevik@32bit.com.tr>
2025-04-27 20:19:50 +0300

    refactor
    refactor

--- src/main/java/com/neysem/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neysem/backend/exception/GlobalExceptionHandler.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
